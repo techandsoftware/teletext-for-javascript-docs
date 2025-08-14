97552e1f7d5876493814135855fcf55547f2e1a5
Rob H <techandsoftware_commits@outlook.com>
2025-08-14 18:15:20 +0100

    add cleanUrls config to remove .html on URLs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Teletext for JS",
   description: "@techandsoftware/teletext and related packages",
+  cleanUrls: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
